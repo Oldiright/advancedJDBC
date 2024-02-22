a4d34071c416bfd033dd7c2522be9e1bb243eebd
oldiright <oliynuk_Dima@ukr.net>
2024-02-22 14:36:46 +0200

    Version without prepareStatement (previous  module)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import db.DatabaseQueryService;
+import db.dao.YoungestOrEldestWorker;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        DatabaseQueryService dbqs = new DatabaseQueryService();
+        List<YoungestOrEldestWorker> result = null;
+
+        result = dbqs.findYoungestAndEldestWorkers();
+
+        System.out.println(result);
+
     }
 }
